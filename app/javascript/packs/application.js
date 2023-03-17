41105ae6420c3f30ae2d69558fc8f1ab134e3cf0
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-03-17 06:07:50 +0000

    [add]bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+//bootstrapを使用するために追加
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application" 
+//FontAwesome導入時に追加
+import '@fortawesome/fontawesome-free/js/all'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
