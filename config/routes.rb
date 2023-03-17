8c9b8976fde5fe69182c8da9d631b0558bc9b7f7
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-03-17 03:08:22 +0000

    [add]退会機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   # ユーザー用
   # URL /customers/sign_in ...
-  devise_for :customers, controllers: {
+  devise_for :users, controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions',
     passwords: "public/passwords"
