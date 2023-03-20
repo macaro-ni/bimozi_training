6b72ed19ac0a20de005c5d5dcb200a6aabb22395
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-03-20 14:04:46 +0000

    [add]hedder,sign_in,userの大枠のレイアウトと機能

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  
+
   #ゲストユーザーの削除防止のため
   before_action :ensure_normal_user, only: :destroy
-  
+
   def ensure_normal_user
     if resource.email == 'guest@example.com'
       redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,26 @@
 class Public::UsersController < ApplicationController
   def show
+    @user=current_user
+
   end
 
   def edit
+    @user=current_user
   end
 
   def update
+    @user=current_user
+    @user.update(user_params)
+    redirect_to users_my_page_path
   end
+
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :address)
+  end
+
 end
