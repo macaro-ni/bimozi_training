8a60d29d3a1d81297c7a106c7931b62c58008606
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-03-22 06:26:04 +0000

    [add]描画機能

--- app/controllers/public/samples_controller.rb
+++ app/controllers/public/samples_controller.rb
@@ -1,10 +1,11 @@
 class Public::SamplesController < ApplicationController
 
  def index
-  @genres=Genre.all
+   @genres=Genre.all
  end
 
   def show
-
+    @letters=Letter.where(genre_id: params[:id])
+    @genre=Genre.find(params[:id])
   end
 end
