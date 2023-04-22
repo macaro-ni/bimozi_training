f844936f1318ba811d0d6f95624536d709af3328
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-04-22 10:39:46 +0000

    [add]seedカタカナ、都道府県

---
+++ db/fixtures/letter_g2.rb
@@ -0,0 +1,233 @@
+
+Letter.seed(
+  {
+    id: 47,
+    genre_id: 2,
+    name: ア,
+  },
+  {
+    id: 48,
+    genre_id: 2,
+    name: イ,
+  },
+  {
+    id: 49,
+    genre_id: 2,
+    name: ウ,
+  },
+    {
+    id: 50,
+    genre_id: 2,
+    name: エ,
+  },
+    {
+    id: 51,
+    genre_id: 2,
+    name: オ,
+  },
+    {
+    id: 52,
+    genre_id: 2,
+    name: カ,
+  },
+    {
+    id: 53,
+    genre_id: 2,
+    name: キ,
+  },
+    {
+    id: 54,
+    genre_id: 2,
+    name: ク,
+  },
+    {
+    id: 55,
+    genre_id: 2,
+    name: ケ,
+  },
+    {
+    id: 56,
+    genre_id: 2,
+    name: コ,
+  },
+    {
+    id: 57,
+    genre_id: 2,
+    name: サ,
+  },
+    {
+    id: 58,
+    genre_id: 2,
+    name: シ,
+  },
+    {
+    id: 59,
+    genre_id: 2,
+    name: ス,
+  },
+    {
+    id: 60,
+    genre_id: 2,
+    name: セ,
+  },
+    {
+    id: 61,
+    genre_id: 2,
+    name: ソ,
+  },
+    {
+    id: 62,
+    genre_id: 2,
+    name: タ,
+  },
+    {
+    id: 63,
+    genre_id: 2,
+    name: チ,
+  },
+    {
+    id: 64,
+    genre_id: 2,
+    name: ツ,
+  },  {
+    id: 65,
+    genre_id: 2,
+    name: テ,
+  },
+    {
+    id: 66,
+    genre_id: 2,
+    name: ト,
+  },
+    {
+    id: 67,
+    genre_id: 2,
+    name: ナ,
+  },
+    {
+    id: 68,
+    genre_id: 2,
+    name: ニ,
+  },
+    {
+    id: 69,
+    genre_id: 2,
+    name: ヌ,
+  },
+    {
+    id: 70,
+    genre_id: 2,
+    name: ネ,
+  },
+  {
+    id: 71,
+    genre_id: 2,
+    name: ノ,
+  },
+    {
+    id: 72,
+    genre_id: 2,
+    name: ハ,
+  },
+    {
+    id: 73,
+    genre_id: 2,
+    name: ヒ,
+  },
+    {
+    id: 74,
+    genre_id: 2,
+    name: フ,
+  },
+  {
+    id: 75,
+    genre_id: 2,
+    name: ヘ,
+  },
+    {
+    id: 76,
+    genre_id: 2,
+    name: ホ,
+  },
+    {
+    id: 77,
+    genre_id: 2,
+    name: マ,
+  },
+    {
+    id: 78,
+    genre_id: 2,
+    name: ミ,
+  },
+  {
+    id: 79,
+    genre_id: 2,
+    name: ム,
+  },
+    {
+    id: 80,
+    genre_id: 2,
+    name: メ,
+  },
+    {
+    id: 81,
+    genre_id: 2,
+    name: モ,
+  },
+    {
+    id: 82,
+    genre_id: 2,
+    name: ヤ,
+  },
+  {
+    id: 83,
+    genre_id: 2,
+    name: ユ,
+  },
+    {
+    id: 84,
+    genre_id: 2,
+    name: ヨ,
+  },
+    {
+    id: 85,
+    genre_id: 2,
+    name: ラ,
+  },
+    {
+    id: 86,
+    genre_id: 2,
+    name: リ,
+  },
+  {
+    id: 87,
+    genre_id: 2,
+    name: ル,
+  },
+    {
+    id: 88,
+    genre_id: 2,
+    name: レ,
+  },
+    {
+    id: 89,
+    genre_id: 2,
+    name: ロ,
+  },
+    {
+    id: 90,
+    genre_id: 2,
+    name: ワ,
+  },
+  {
+    id: 91,
+    genre_id: 2,
+    name: ヲ,
+  },
+    {
+    id: 92,
+    genre_id: 2,
+    name: ン,
+  },
+
+  )
